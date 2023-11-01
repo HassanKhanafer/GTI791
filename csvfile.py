66b934e93002a7d22b7369e061532d27755e17d6
HassanKhanafer <hassankhanafer2011@hotmail.com>
2023-11-01 11:15:56 -0400

    second push fixed the project_name problem

--- csvfile.py
+++ csvfile.py
@@ -1,9 +1,8 @@
-import csv
-import json
+import pandas as pd
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import simpledialog
-import os
+import json
 
 def select_file():
     root = tk.Tk()
@@ -25,42 +24,31 @@ def extract_info(payload, keys_to_extract):
     except json.JSONDecodeError:
         return {key: '' for key in keys_to_extract}
 
-def process_csv(file_path, output_folder, output_file_name, specific_project=None):
-    keys_to_extract = ["url", "id", "node_id", "html_url", "diff_url", "patch_url", "issue_url", "number", "state", "locked", "title"]
-    csv.field_size_limit(2147483647)  # Augmenter la limite de taille des champs CSV
-    with open(file_path, mode='r', encoding='utf-8') as file:
-        csv_reader = csv.DictReader(file)
-        
-    for row in csv_reader:
-            projectname = row.get('project name', 'N/A')
-
-            if specific_project is None or projectname.lower() == specific_project.lower():
-                extracted_info = extract_info(row.get('payload', '{}'), keys_to_extract)
-                extracted_info["project_name"] = projectname  # Ajoutez le nom du projet à extracted_info
-                output_file_path = os.path.join(output_folder, f"{output_file_name}.csv")
-                print("Output File Path:", output_file_path)  # Débogage
-                try:
-                    with open(output_file_path, mode='a', newline='', encoding='utf-8') as output_file:
-                        writer = csv.DictWriter(output_file, fieldnames=["project_name"] + keys_to_extract)
-                        if output_file.tell() == 0:
-                            writer.writeheader()
-                        writer.writerow(extracted_info)
-                except Exception as e:
-                    print(f"Error writing to file: {e}")
-
 def main():
+    csv_file_path = select_file()
     output_folder = select_output_folder()
-    if output_folder:
-        csv_file_path = select_file()
-        if csv_file_path:
-            print(f"Processing file: {csv_file_path}")
-            output_file_name = simpledialog.askstring("Input", "Entrez le nom du fichier de sortie:")
-            specific_project = simpledialog.askstring("Input", "Entrez le nom du projet à rechercher (laissez vide pour tous les projets):")
-            process_csv(csv_file_path, output_folder, output_file_name, specific_project)
-        else:
-            print("No file selected. Exiting.")
+    
+    if csv_file_path and output_folder:
+        print(f"Processing file: {csv_file_path}")
+        output_file_name = simpledialog.askstring("Input", "Enter the output file name:")
+        
+        # Create an empty DataFrame
+        df = pd.DataFrame(columns=["project_name", "url", "id", "node_id", "html_url", "diff_url", "patch_url", "issue_url", "number", "state", "locked", "title"])
+        
+        with open(csv_file_path, mode='r', encoding='utf-8') as file:
+            csv_reader = pd.read_csv(file)
+            for _, row in csv_reader.iterrows():
+                payload = row.get('payload', '{}')
+                project_name = row.get('project_name', '').strip()
+                extracted_info = extract_info(payload, df.columns[1:])  # Exclude 'project_name' from extraction
+                extracted_info['project_name'] = project_name
+                df = pd.concat([df, pd.DataFrame([extracted_info])], ignore_index=True)
+        
+        output_file_path = f"{output_folder}/{output_file_name}.csv"
+        df.to_csv(output_file_path, index=False)
+        print("Processing complete. Output file saved.")
     else:
-        print("No output folder selected. Exiting.")
+        print("No file selected. Exiting.")
 
 if __name__ == "__main__":
     main()
